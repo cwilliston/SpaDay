3ab1b41f30f103d98eb3cd825147a42b88dc9f64
cwilliston <carolynnwilliston@gmail.com>
2022-10-19 20:15:50 -0500

    ch15 studio complete

--- SpaDay/Controllers/UserController.cs
+++ SpaDay/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SpaDay.Models;
+using SpaDay.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,27 +18,41 @@ namespace SpaDay.Controllers
             return View();
         }
 
-        public IActionResult Add()
+        public IActionResult Add(AddUserViewModel addUserViewModel)
         {
-            return View();
+
+            return View(addUserViewModel);
         }
 
         [HttpPost]
-        [Route("/user")]
-        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        [Route("/user")] 
+        public IActionResult SubmitAddUserForm(AddUserViewModel addUserViewModel)
         {
-            if (newUser.Password == verify)
+            if (ModelState.IsValid)
             {
-                ViewBag.user = newUser;
-                return View("Index");
+
+
+                if (addUserViewModel.Password == addUserViewModel.VerifyPassword)
+                {
+                    
+                    User newUser = new User
+                    {
+                        Username = addUserViewModel.Username,
+                        Email = addUserViewModel.Email,
+                        Password = addUserViewModel.Password
+                    };
+                    
+                    return View("Index", newUser);
+                }
+                else
+                {
+                    ViewBag.error = "Passwords do not match! Try again!";
+
+                    return View("Add");
+                }
             }
-            else
-            {
-                ViewBag.error = "Passwords do not match! Try again!";
-                ViewBag.userName = newUser.Username;
-                ViewBag.eMail = newUser.Email;
-                return View("Add");
-            }
+
+            return View("Add");
         }
 
     }
---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(15, MinimumLength = 5)]
+        public string Username { get; set; }
+        
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Verify password is required")]
+        public string VerifyPassword { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+        
+
+
+    }
+}
